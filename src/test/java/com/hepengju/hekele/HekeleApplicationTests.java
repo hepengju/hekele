825c557e9599f5242565663014a6e58d086d9233
何鹏举 <he_pengju@163.com>
2019-12-19 22:54:23 +0800

    初始化项目结构

---
+++ src/test/java/com/hepengju/hekele/HekeleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hepengju.hekele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HekeleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

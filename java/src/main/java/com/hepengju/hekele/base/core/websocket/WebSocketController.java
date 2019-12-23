74044718ce252d4e4e9db3de6051948770fba4d5
何鹏举 <hepengju@chengtay.com>
2019-12-23 13:00:16 +0800

    WebSocket注释掉, 有空再进行优化处理

--- java/src/main/java/com/hepengju/hekele/base/core/websocket/WebSocketController.java
+++ java/src/main/java/com/hepengju/hekele/base/core/websocket/WebSocketController.java
@@ -1,31 +1,31 @@
-package com.hepengju.hekele.base.core.websocket;
-
-import com.topcheer.oss.base.common.R;
-import com.topcheer.oss.base.util.ShiroUtil;
-import com.topcheer.oss.base.util.WebSocketUtil;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * 前端界面发送消息控制器
- */
-@RestController
-@RequestMapping("/sys/websocket")
-public class WebSocketController {
-
-    @PostMapping("/sendMessage")
-    public R sendMessage(String message,@RequestParam(name = "account",required = false) String account){
-        String name = ShiroUtil.getActiveUser().getUser().getName();
-        WebSocketUtil.sendMessage(WebSocketHandler.getAccountSessionMap(), name + ":" + message, account);
-        return R.ok();
-    }
-
-    @PostMapping("/sendMessageJSR356")
-    public R sendMessageJSR356(String message,@RequestParam(name = "account",required = false) String account){
-        String name = ShiroUtil.getActiveUser().getUser().getName();
-        WebSocketUtil.sendAccountMessage(WebSocketHandlerJSR356.getAccountSessionMap(), name + ":" + message, account);
-        return R.ok();
-    }
-}
+//package com.hepengju.hekele.base.core.websocket;
+//
+//import com.topcheer.oss.base.common.R;
+//import com.topcheer.oss.base.util.ShiroUtil;
+//import com.topcheer.oss.base.util.WebSocketUtil;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * 前端界面发送消息控制器
+// */
+//@RestController
+//@RequestMapping("/sys/websocket")
+//public class WebSocketController {
+//
+//    @PostMapping("/sendMessage")
+//    public R sendMessage(String message,@RequestParam(name = "account",required = false) String account){
+//        String name = ShiroUtil.getActiveUser().getUser().getName();
+//        WebSocketUtil.sendMessage(WebSocketHandler.getAccountSessionMap(), name + ":" + message, account);
+//        return R.ok();
+//    }
+//
+//    @PostMapping("/sendMessageJSR356")
+//    public R sendMessageJSR356(String message,@RequestParam(name = "account",required = false) String account){
+//        String name = ShiroUtil.getActiveUser().getUser().getName();
+//        WebSocketUtil.sendAccountMessage(WebSocketHandlerJSR356.getAccountSessionMap(), name + ":" + message, account);
+//        return R.ok();
+//    }
+//}

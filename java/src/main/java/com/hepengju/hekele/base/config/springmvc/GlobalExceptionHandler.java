cd56442d17dc5f0bf8c367e63d5433f9e6363025
何鹏举 <he_pengju@163.com>
2019-12-24 23:35:12 +0800

    M类配置, 读取, 测试, 异常处理等完善

--- java/src/main/java/com/hepengju/hekele/base/config/springmvc/GlobalExceptionHandler.java
+++ java/src/main/java/com/hepengju/hekele/base/config/springmvc/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.hepengju.hekele.base.config.springmvc;
 
+import com.hepengju.hekele.base.constant.HeConst;
+import com.hepengju.hekele.base.core.M;
 import com.hepengju.hekele.base.core.R;
 import com.hepengju.hekele.base.core.exception.BeanValidException;
 import com.hepengju.hekele.base.core.exception.HeException;
+import com.hepengju.hekele.base.util.StackUtil;
 import com.hepengju.hekele.base.util.ValidUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindException;
@@ -22,7 +25,7 @@ public class GlobalExceptionHandler {
 
 
     /**
-     * 自动的数据绑定验证异常拦截
+     * SpringMVC数据绑定异常(application/x-www-form-urlencoded)
      *
      * <pre>
      * 接口上的Bean验证，需要在参数前加上@Valid或Spring的 @Validated注解，这两种注释都会导致应用标准Bean验证。
@@ -34,13 +37,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BindException.class)
     public R handleBindException(BindException e) {
         ObjectError objectError = e.getAllErrors().get(0);
-        return R.err(getObjectErrorMessage(objectError)).setCode(10001);
+        return R.err(getObjectErrorMessage(objectError)).setCode(HeConst.ErrorCode.SPRINGMVC_FORM_BIND_ERROR);
     }
 
+    /**
+     * SpringMVC数据绑定异常(application/x-www-form-urlencoded)
+     */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public R handleBindException(MethodArgumentNotValidException e) {
         ObjectError objectError = e.getBindingResult().getAllErrors().get(0);
-        return R.err(getObjectErrorMessage(objectError)).setCode(10002);
+        return R.err(getObjectErrorMessage(objectError)).setCode(HeConst.ErrorCode.SPRINGMVC_JSON_BIND_ERROR);
     }
 
     private String getObjectErrorMessage(ObjectError objectError) {
@@ -54,22 +60,26 @@ public class GlobalExceptionHandler {
     }
 
     /**
-     * 手动的数据绑定验证异常拦截
+     * 手动执行的Hibernate Bean Validation验证异常
      */
     @ExceptionHandler(BeanValidException.class)
     public R handleBeanValidException(BeanValidException e) {
         log.error(e.getMessage(), e);
-        return R.err(e.getMessage()).setCode(10003);
+        return R.err(e.getMessage()).setCode(HeConst.ErrorCode.HIBERNATE_BEAN_VALID_ERROR);
     }
 
     /**
      * 自定义异常
+     *
+     * 1. 记录错误异常
+     * 2. 如果是浏览器端, 则加入错误详细信息(移动端不加入)
      */
     @ExceptionHandler(HeException.class)
     public R handleHeException(HeException e) {
-        // TODO 特殊处理
         log.error(e.getMessage(), e);
-        return R.err(e.getMessage());
+        String stackTrace = StackUtil.getStackTrace(e);
+        String message = M.get(e.getMessage(), e.getErrFormatArr());
+        return R.err(message).setExtra(stackTrace);
     }
 
     /**
--- java/src/main/java/com/hepengju/hekele/base/constant/HeConst.java
+++ java/src/main/java/com/hepengju/hekele/base/constant/HeConst.java
@@ -4,6 +4,19 @@ package com.hepengju.hekele.base.constant;
  * 常量类
  */
 public interface HeConst {
+
     String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
     String DATE_FORMAT      = "yyyy-MM-dd";
+
+    /**
+     * 错误代码, 其他的错误代码, 参看 he.message.json
+     */
+    interface ErrorCode {
+        int SUCCESS                    = 0;
+        int UNKOWN_ERROR               = -1;
+        int NO_LOGIN_ERROR             = 10000;
+        int SPRINGMVC_FORM_BIND_ERROR  = 10001;
+        int SPRINGMVC_JSON_BIND_ERROR  = 10002;
+        int HIBERNATE_BEAN_VALID_ERROR = 10003;
+    }
 }

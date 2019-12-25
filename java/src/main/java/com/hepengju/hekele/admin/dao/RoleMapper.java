69826be6c50d5a1dee174e733a983f4c081e3cf4
何鹏举 <he_pengju@163.com>
2019-12-26 00:11:42 +0800

    角色测试类, 发现的BUG修复

--- java/src/main/java/com/hepengju/hekele/admin/dao/RoleMapper.java
+++ java/src/main/java/com/hepengju/hekele/admin/dao/RoleMapper.java
@@ -12,9 +12,9 @@ import java.util.List;
 @Mapper
 public interface RoleMapper extends HeMapper<Role> {
 
-    @Select("SELECT * FROM z10_user WHERE delete_flag = '1' AND user_id IN (SELECT user_id FROM z11_user_role WHERE role_id = #{id}")
+    @Select("SELECT * FROM z10_user WHERE delete_flag = '1' AND user_id IN (SELECT user_id FROM z11_user_role WHERE role_id = #{id})")
     List<User> listUser(String id);
 
-    @Select("SELECT * FROM z10_menu WHERE delete_flag = '1' AND menu_id IN (SELECT menu_id FROM z11_role_menu WHERE role_id = #{id}")
+    @Select("SELECT * FROM z10_menu WHERE delete_flag = '1' AND menu_id IN (SELECT menu_id FROM z11_role_menu WHERE role_id = #{id})")
     List<Menu> listMenu(String id);
 }

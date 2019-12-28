0eb0bf2d02224e08f51761f37d8bd4deb8334d2e
何鹏举 <he_pengju@163.com>
2019-12-28 22:10:30 +0800

    追加通用方法, CountByTwoColumnValue; 删除禁用/启动ById

--- java/src/main/java/com/hepengju/hekele/base/core/HeService.java
+++ java/src/main/java/com/hepengju/hekele/base/core/HeService.java
@@ -17,20 +17,25 @@ public class HeService<M extends HeMapper<T>, T> extends ServiceImpl<M,T> {
 
     @Autowired protected HeMapper<T> heMapper;
 
+    // 查询某列指定值的个数
+    public long countByColumnValue(String columnName, String columnValue, String idValue){ return heMapper.countByColumnValue(columnName, columnValue, idValue); }
+    public long countByTwoColumnValue(String columnName1, String columnValue1, String columnName2, String columnValue2, String idValue) {
+        return heMapper.countByTwoColumnValue(columnName1, columnValue1, columnName2, columnValue2, idValue);
+    }
+
     // 启用, 禁用
-    public R enableById(Serializable id) { return R.ok().addData(heMapper.enableById(id));};
-    public R disableById(Serializable id) { return R.ok().addData(heMapper.disableById(id));}
     public R enableBatchByIds (Collection<? extends Serializable> idList) { return R.ok().addData(CollectionUtils.isEmpty(idList) ? 0 : heMapper.enableBatchByIds(idList)); }
     public R disableBatchByIds(Collection<? extends Serializable> idList) { return R.ok().addData(CollectionUtils.isEmpty(idList) ? 0 : heMapper.disableBatchByIds(idList));}
 
-    // 查询某列指定值的个数
-    public long countByColumnValue(String columnName, String columnValue, String idValue){ return heMapper.countByColumnValue(columnName, columnValue, idValue); }
+    // 验证唯一性
     public void validUnique(String columnName, String columnValue, String idValue, String err) {
         long count = heMapper.countByColumnValue(columnName, columnValue, idValue);
         if (count > 0) throw new HeException(err);
     }
-
-
+    public void validUnique(String columnName1, String columnValue1, String columnName2, String columnValue2, String idValue, String err) {
+        long count = heMapper.countByTwoColumnValue(columnName1, columnValue1, columnName2, columnValue2, idValue);
+        if (count > 0) throw new HeException(err);
+    }
 
     // 查询自动选择是否分页
     public R listR(Long pageNum, Long pageSize) {return listR(pageNum, pageSize, null);}

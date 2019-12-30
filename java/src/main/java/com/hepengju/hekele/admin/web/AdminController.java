532176ac3992f684b0e406887cca9266a08ef822
何鹏举 <he_pengju@163.com>
2019-12-30 21:31:15 +0800

    图标, 重启及去掉开发者工具

--- java/src/main/java/com/hepengju/hekele/admin/web/AdminController.java
+++ java/src/main/java/com/hepengju/hekele/admin/web/AdminController.java
@@ -1,19 +1,22 @@
 package com.hepengju.hekele.admin.web;
 
 
-import com.hepengju.hekele.base.util.ExecUtil;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.hepengju.hekele.base.core.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 /**
  * 管理员控制
  *
  * @author he_pe 2019-12-25
  */
-@Controller
+@Slf4j
+@RestController
 @RequestMapping("/admin")
 public class AdminController {
 
@@ -23,9 +26,9 @@ public class AdminController {
      * Git拉取最新版本, 然后重启
      */
     @GetMapping("/gitPullThenRestart")
-    public String gitPullThenRestart() {
-        ExecUtil.execCommand(restartShell);
-        return "index";
+    public R gitPullThenRestart() throws IOException {
+        Runtime.getRuntime().exec(restartShell);
+        return R.ok().setErrmsg("正在重新启动, 请稍等");
     }
 
 }

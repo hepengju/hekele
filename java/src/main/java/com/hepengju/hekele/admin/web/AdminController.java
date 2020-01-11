c24425e21d2619d5a96027506356e776810e0bc2
何鹏举 <he_pengju@163.com>
2020-01-11 15:23:01 +0800

    SpringBoot下载文件的处理

--- java/src/main/java/com/hepengju/hekele/admin/web/AdminController.java
+++ java/src/main/java/com/hepengju/hekele/admin/web/AdminController.java
@@ -9,13 +9,14 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * 管理员控制
@@ -27,6 +28,8 @@ import java.io.InputStream;
 @RestController
 @RequestMapping("/admin/super")
 public class AdminController {
+
+    private ResourceLoader resourceLoader = new DefaultResourceLoader();
 
     @Value("${hekele.restart.shell}")
     private String restartShell;
@@ -42,11 +45,8 @@ public class AdminController {
     @ApiOperation("下载hekele最新数据库设计")
     @GetMapping("downloadHekeleXlsm")
     public void downloadHekeleXlsm(HttpServletResponse res) throws IOException {
-        try(InputStream is = this.getClass().getClassLoader().getResourceAsStream("public/db/hekele.xlsm")){
-            byte[] bytes = new byte[is.available()];
-            is.read(bytes);
-            WebUtil.handleFileDownload("hekele-" + Now.yyyyMMddHHmmss() + ".xlsm", bytes);
-        };
+        WebUtil.handleFileDownload("hekele-" + Now.yyyyMMddHHmmss() + ".xlsm",
+                resourceLoader.getResource("classpath:public/db/hekele.xlsm").getInputStream());
     }
 
 }
--- java/src/main/java/com/hepengju/hekele/base/util/WebUtil.java
+++ java/src/main/java/com/hepengju/hekele/base/util/WebUtil.java
@@ -17,6 +17,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
@@ -42,11 +43,7 @@ public class WebUtil implements ApplicationContextAware {
 	 * 处理文件下载的通用方法
 	 */
 	public static void handleFileDownload(String attachment){
-		handleFileDownload(attachment, null);
-	}
-	public static void handleFileDownload(String attachment, byte[] byteArray){
 		String fileName = attachment;
-
 		// 这种方式: 谷歌/火狐OK, IE/Edge不行
 		//try {fileName = new String(fileName.getBytes("UTF-8"),"ISO-8859-1");} catch (UnsupportedEncodingException e) {}
 
@@ -55,7 +52,6 @@ public class WebUtil implements ApplicationContextAware {
 		HttpServletResponse response = WebUtil.getHttpServletResponse();
 		response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
 		response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"; filename*=utf-8''" + fileName);
-
 
 		// 保留原始的文件名, 便于前端解析 --> 需要使用; 两种都设置下
 		// var filename = resp.headers['original-filename'];
@@ -66,10 +62,23 @@ public class WebUtil implements ApplicationContextAware {
 		// 以下两个也加入, 以便可能的使用
 		response.setHeader("Original-Filename-Java", attachment);
 		response.setHeader("Original-Filename-Encode", fileName);
+	}
 
+	public static void handleFileDownload(String attachment, InputStream inputStream) {
+		try (InputStream is = inputStream) {
+			byte[] bytes = new byte[is.available()];
+			is.read(bytes);
+			WebUtil.handleFileDownload(attachment, bytes);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void handleFileDownload(String attachment, byte[] byteArray){
+		handleFileDownload(attachment);
 		if(byteArray != null){
 			try {
-				response.getOutputStream().write(byteArray);
+				WebUtil.getHttpServletResponse().getOutputStream().write(byteArray);
 			} catch (IOException e) {
 				throw new HeException(e);
 			}

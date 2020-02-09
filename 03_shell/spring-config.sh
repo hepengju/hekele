12345296d1996dda48a8fdec926965bbf4721a0e
何鹏举 <he_pengju@163.com>
2020-02-09 13:32:28 +0800

    修改参数的位置

--- 03_shell/spring-config.sh
+++ 03_shell/spring-config.sh
@@ -6,7 +6,7 @@ do
   sleep 2
 done
 
-nohup java  -jar -Xms256M -Xmx512M --eureka.instance.ip-address=101.132.97.183 $appName >> /dev/null 2>&1 &
+nohup java  -jar -Xms256M -Xmx512M $appName --eureka.instance.ip-address=101.132.97.183 >> /dev/null 2>&1 &
 
 tail -f ~/app/log/spring-config/spring-config.log
 

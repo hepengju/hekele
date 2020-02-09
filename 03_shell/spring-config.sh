20778e8b08d2242f7344780f032cdff1be0fd0bd
何鹏举 <he_pengju@163.com>
2020-02-09 21:35:44 +0800

    判断日志文件是否存在，不存在等待两秒再tail -f

--- 03_shell/spring-config.sh
+++ 03_shell/spring-config.sh
@@ -1,5 +1,6 @@
 appName=spring-config
 jarName=$appName.jar
+logFile=$HOME/app/log/$appName/$appName.log
 
 ps -ef | grep $jarName | awk '{print $2}' | while read pid
 do
@@ -9,4 +10,7 @@ done
 
 nohup java  -jar -Xms256M -Xmx512M $jarName --eureka.instance.ip-address=101.132.97.183 >> /dev/null 2>&1 &
 
+if [ ! -f logFile ]; then
+  sleep 2
+fi
 tail -f ~/app/log/$appName/$appName.log

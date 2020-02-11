7f424cf729c93683b710ee55658fef4a6a656bbb
何鹏举 <hepengju@chengtay.com>
2020-02-11 23:51:58 +0800

    添加activiti常量, 并优化注释

--- 11_base/base-nodb/src/main/java/com/hepengju/hekele/base/cloud/FeignClientConst.java
+++ 11_base/base-nodb/src/main/java/com/hepengju/hekele/base/cloud/FeignClientConst.java
@@ -11,9 +11,11 @@ public interface FeignClientConst {
     String SPRING_CONFIG  = "spring-config"  ; // 配置中心
     String SPRING_MONITOR = "spring-monitor" ; // 监控服务
 
-    String ADMIN_SERVICE   = "admin-service"   ; // 人员角色菜单
-    String FILE_SERVICE    = "file-service"    ; // 文件管理
-    String MESSAGE_SERVICE = "message-service" ; // 消息中共新
-    String DATA_SERVICE    = "data-service"    ; // 模拟数据
+
+    String ADMIN_SERVICE    = "admin-service"   ; // 人员角色菜单
+    String FILE_SERVICE     = "file-service"    ; // 文件管理
+    String MESSAGE_SERVICE  = "message-service" ; // 消息中心
+    String DATA_SERVICE     = "data-service"    ; // 模拟数据
+    String ACTIVITI_SERVICE = "data-service"    ; // 工作流服务
 
 }
--- 11_base/base-nodb/src/main/java/com/hepengju/hekele/base/cloud/FeignFallbackUtil.java
+++ 11_base/base-nodb/src/main/java/com/hepengju/hekele/base/cloud/FeignFallbackUtil.java
@@ -8,9 +8,10 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 /**
- * 接口调用失败的通用处理类
+ * 接口调用失败的通用简单粗暴处理类
  *
- * <br> 打印下方法与参数，返回null
+ * <br> 备注: 仅仅打印下调用服务的方法与参数，返回null
+ * <br> 原因: 项目中很多时候的降级处理就没写, 或有些就打印个日志. 或服务名修改了, 新增方法了, 每次新增无意义代码. 此时可以使用此类
  *
  * @author hepengju
  */
@@ -25,8 +26,7 @@ public class FeignFallbackUtil {
                 , new InvocationHandler() {
                     @Override
                     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                        log.error("当你看到这条信息，说明服务[{}.{}.{}]已经停止或不可访问，参数为：{}",
-                                serviceName, feignClientInterface.getSimpleName(), method.getName(), args, cause);
+                        log.error("当你看到这条信息，说明服务[{}.{}.{}]已经停止或不可访问，参数为：{}", serviceName, feignClientInterface.getSimpleName(), method.getName(), args, cause);
                         return null;
                     }
                 });

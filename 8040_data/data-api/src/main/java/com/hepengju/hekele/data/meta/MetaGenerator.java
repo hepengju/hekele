b6531dca242009241224260a00212dc484c68395
何鹏举 <hepengju@chengtay.com>
2020-02-21 14:14:49 +0800

    生成器元数据的Swagger注释

--- 8040_data/data-api/src/main/java/com/hepengju/hekele/data/meta/MetaGenerator.java
+++ 8040_data/data-api/src/main/java/com/hepengju/hekele/data/meta/MetaGenerator.java
@@ -1,6 +1,8 @@
 package com.hepengju.hekele.data.meta;
 
 import com.hepengju.hekele.data.generator.Generator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.apache.commons.beanutils.BeanUtils;
 
@@ -10,22 +12,22 @@ import java.util.List;
 /**
  * 生成器的配置值
  */
-@Data
+@Data @ApiModel("生成器元数据")
 public class MetaGenerator {
 
     public static int SAMPLE_SIZE = 10;
 
     @NotNull
-    private String className;
-    private String desc;
-    private String type;
-    private List<Object> sampleData;
+    @ApiModelProperty("全类名") private String className;
+    @ApiModelProperty("描述") private String desc;
+    @ApiModelProperty("分类") private String type;
+    @ApiModelProperty("样例") private List<Object> sampleData;
     //-----------------------------------------------------------
-    private String  min;       // 最小值, 个人情况下表示字符串的最小长度
-    private String  max;       // 最大值, 个别情况下表示字符串的最大长度
-    private Integer scale;     // 保留小数位数
-    private String  code;      // 代码值, 逗号分隔
-    private String  codeMulti; // 20200210 代码值, 可以选择是否多选 (Y/N)  Boolean类型的前后端传输容易错误
+    @ApiModelProperty("最小值")   private String  min;       // 最小值, 个人情况下表示字符串的最小长度
+    @ApiModelProperty("最大值")   private String  max;       // 最大值, 个别情况下表示字符串的最大长度
+    @ApiModelProperty("小数位数") private Integer scale;     // 保留小数位数
+    @ApiModelProperty("代码枚举") private String  code;      // 代码值, 逗号分隔
+    @ApiModelProperty("代码多选") private String  codeMulti; // 20200210 代码值, 可以选择是否多选 (Y/N)  Boolean类型的前后端传输容易错误
     //-----------------------------------------------------------
 
     public Generator toGenerator(){

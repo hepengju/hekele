26736f8e0eb65e56a63a1b9ebbf1839b54d5f994
何鹏举 <hepengju@chengtay.com>
2020-02-22 00:06:46 +0800

    zipkin: 7040 --> 7060, 加入turbine工程

--- 7050_zuul/src/main/java/com/hepengju/hekele/SpringZuulApp.java
+++ 7050_zuul/src/main/java/com/hepengju/hekele/SpringZuulApp.java
@@ -2,8 +2,10 @@ package com.hepengju.hekele;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableHystrix
 @EnableZuulProxy
 @SpringBootApplication
 public class SpringZuulApp {

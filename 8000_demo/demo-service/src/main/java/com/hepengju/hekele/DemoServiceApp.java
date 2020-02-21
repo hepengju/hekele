40df63d7259ec6b62fde8f996c53e66aa0403132
何鹏举 <hepengju@chengtay.com>
2020-02-22 00:01:42 +0800

    @EnableHystrix

--- 8000_demo/demo-service/src/main/java/com/hepengju/hekele/DemoServiceApp.java
+++ 8000_demo/demo-service/src/main/java/com/hepengju/hekele/DemoServiceApp.java
@@ -2,9 +2,11 @@ package com.hepengju.hekele;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableHystrix
 @EnableZuulProxy
 @EnableFeignClients
 @SpringBootApplication

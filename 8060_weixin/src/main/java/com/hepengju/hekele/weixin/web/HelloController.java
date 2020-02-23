6becde9c9d649192b8de9f16915397f1fd241c51
何鹏举 <hepengju@chengtay.com>
2020-02-23 23:23:45 +0800

    微信的stripPrefix改为false

--- 8060_weixin/src/main/java/com/hepengju/hekele/weixin/web/HelloController.java
+++ 8060_weixin/src/main/java/com/hepengju/hekele/weixin/web/HelloController.java
@@ -1,9 +1,11 @@
 package com.hepengju.hekele.weixin.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/wx")
 public class HelloController {
 
     @GetMapping("/hello")

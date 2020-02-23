cf443505429ce2f6462d0e1060b6d40bcae8e3d2
何鹏举 <hepengju@chengtay.com>
2020-02-23 16:47:52 +0800

    @EnableZuulProxy 已经启用了Hystrix, 首页变更

--- 7100_zuul/src/main/java/com/hepengju/hekele/SpringZuulApp.java
+++ 7100_zuul/src/main/java/com/hepengju/hekele/SpringZuulApp.java
@@ -2,10 +2,8 @@ package com.hepengju.hekele;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableHystrix
 @EnableZuulProxy
 @SpringBootApplication
 public class SpringZuulApp {

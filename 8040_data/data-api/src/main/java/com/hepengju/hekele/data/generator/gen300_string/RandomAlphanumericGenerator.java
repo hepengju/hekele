a7dc589d2737bb960ed7141dfd6d33ac76cb3dc7
何鹏举 <hepengju@chengtay.com>
2020-02-23 22:32:14 +0800

    微信项目初步OK

--- 8040_data/data-api/src/main/java/com/hepengju/hekele/data/generator/gen300_string/RandomAlphanumericGenerator.java
+++ 8040_data/data-api/src/main/java/com/hepengju/hekele/data/generator/gen300_string/RandomAlphanumericGenerator.java
@@ -7,7 +7,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.core.annotation.Order;
 
 @ApiModel("随机字母数字生成器") @Data @Order(305)
-public class RandomAlphanumeric extends AbstractStringGenerator {
+public class RandomAlphanumericGenerator extends AbstractStringGenerator {
 
     private int min = 6;
     private int max = 6;

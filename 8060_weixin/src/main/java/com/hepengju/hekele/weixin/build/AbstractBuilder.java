f4b7d048baad72568ade32ef8376f0cab5d8a03d
何鹏举 <hepengju@chengtay.com>
2020-02-23 23:18:27 +0800

    我去, build目录名称特殊, 竟然被忽略了!!

---
+++ 8060_weixin/src/main/java/com/hepengju/hekele/weixin/build/AbstractBuilder.java
@@ -0,0 +1,9 @@
+package com.hepengju.hekele.weixin.build;
+
+import me.chanjar.weixin.mp.api.WxMpService;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
+
+public abstract class AbstractBuilder {
+    public abstract WxMpXmlOutMessage build(String content, WxMpXmlMessage wxMessage, WxMpService service);
+}

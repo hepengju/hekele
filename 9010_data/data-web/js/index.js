bae68e47b9b8820e58a5d5ba4bbb2da5d2b750b4
何佳欢 <he__jiahuan@163.com>
2020-02-26 21:30:06 +0800

    更新

--- 9010_data/data-web/js/index.js
+++ 9010_data/data-web/js/index.js
@@ -1,8 +1,9 @@
 window.onload = function() {
-
+	let _this = this
 	// 配置项目地址及响应拦截器
 	axios.defaults.baseURL = 'http://ali.hepengju.com:8040/data/';
-	//axios.defaults.baseURL = 'http://localhost:8040/data/';
+	axios.defaults.headers.post['Content-Type'] = 'application/json';
+
 	axios.interceptors.response.use(function(response) {
 		// 下载文件的处理
 		if (response.config.responseType == 'blob') {
@@ -36,12 +37,16 @@ window.onload = function() {
 
 		// 出现错误的处理
 		if (response.headers['content-type'] == 'application/json' && response.data.errCode != 0) {
+			console.log(1234);
 			alert(response.data.errMsg)
 		}
 
 		return response
 	}, function(error) {
 		alert("服务器连接失败");
+		// this.$Message.error({
+		// 	content:'服务器连接失败！'
+		// });
 		return Promise.reject(error);
 	});
 
@@ -52,6 +57,8 @@ window.onload = function() {
 			return {
 				showTable:true,
 				disabled:false,
+				genClass:['日期生成器','数字生成器','字符生成器','定制生成器'],
+				genLength:0, //生成器个数
 				btnList:[
 					{label: '保存',       type:'info'},
 					{label: '刷新',       type:'primary'},
@@ -66,24 +73,25 @@ window.onload = function() {
 					'excel'
 				],
 				configForm:{
+					columnTitle:null,
 					min: null,
 					max: null,
 					code: null,
-					scale: null,
-					codeMulti: null
+					format: null,
+					codeMulti: null,
+					prefix:null,
+					suffix:null,
 				},
 				downloadNumber:1000,
 				columns: [],
 				data: [],
 				dataTmp:[],
 				dataList:[],
-				// characterList: [],
-				// dateList: [],
-				// numberList: [],
-				// customList: [],
 				isDelete:false,
 				selectColumn:{}, //选中的列
 				UUID: 0, // 列的唯一键
+				metaList: [],  //  json数组
+				columnNames:[],  // 列名
 			}
 		},
 		methods: {
@@ -101,33 +109,74 @@ window.onload = function() {
 				}
 			},
 
+			refresh() {
+				this.getCurrentColumns(this.columns);
+				let colName = '';
+				let key ='';
+				let params = {'metaList': this.metaList};
+				params = JSON.stringify(params);
+				axios.post('refreshTable',params).then(res=>{
+					let newData = res.data.data;
+					this.columns.forEach(item => {
+						for (let i = 0 ; i < newData.length ; i++) {
+							colName = item.title;
+							key= item.UUID;
+							this.data[i][colName] = newData[i][key]
+						}
+						console.log(key);
+					})
+				})
+			},
+
 			// 显示配置
 			showConfig(item) {
-				console.log(item.column);
+				console.log(item);
+				// item.column['className'] = 'demo-table-info-column';
+				item.column.className = 'demo-table-info-column';
+				// document.querySelector('.ivu-table-cell').style.color = '#678908'
+				// console.log(this.$refs.data_table);
+				// this.$refs.data_table.$el.style.backgroundColor = '#aaa'
 				for (let key in this.configForm){
 					this.configForm[key] = item.column[key];
+					this.configForm.columnTitle = item.column.title;
 					if (item.column[key] == null) {
 						// this.disabled = true
 					}
 				}
 				this.selectColumn = item.column;
 			},
+
 			// 保存配置
 			save() {
-				console.log(this.data);
+				if (this.columns.length == 0) {
+					this.$Message.warning({
+						content:'暂无生成器可保存，请选择所需生成器！'
+					});
+					return
+				}
+				this.columns[this.selectColumn.UUID].title = this.configForm.columnTitle;
 				for (let item in this.configForm){
-					if( this.configForm[item] == null) {
+					if (this.configForm[item] == null) {
 						this.configForm[item] = ''
 					}
 				}
+				let otherObj = {
+					columnKey: this.selectColumn.UUID,
+					columnName:this.selectColumn.name,
+					name: this.selectColumn.name,
+					type: this.selectColumn.type,
+				};
+				let metaObj = Object.assign({},otherObj,this.configForm);
+				this.metaList.push(metaObj);
 				let key = this.selectColumn.title;
-				let _this = this;
-				let params = `className=${this.selectColumn.className}&code=${this.configForm.code}&min=${this.configForm.min}&max=${this.configForm.max}&scale=${this.configForm.scale}&codeMulti=${this.configForm.codeMulti}&sampleSize=10`;
-				axios.post('getSampleData',params)
-					.then(function (res) {
+				let UUID = this.selectColumn.UUID;
+				let params = JSON.stringify({'metaList':this.metaList});
+				axios.post('refreshTable',params)
+					.then(res => {
+						this.metaList = [];
 						let newData = res.data.data;
 						for (let i = 0 ; i < newData.length ; i++) {
-							_this.data[i][key] = newData[i]
+							this.data[i][key] = newData[i][UUID]
 						}
 					})
 			},
@@ -136,6 +185,7 @@ window.onload = function() {
 				for (let item in this.configForm){
 					this.configForm[item] = this.selectColumn[item]
 				}
+				this.configForm.columnTitle = this.selectColumn.title
 			},
 
 			// 拖拽
@@ -144,23 +194,14 @@ window.onload = function() {
 				this.data.splice(b, 1, ...this.data.splice(a, 1, this.data[b]));
 			},
 
-			// 刷新样例数据
-			refresh() {
-				let _this = this;
-				axios.post('refreshSampleData')
-					.then(function (data) {
-						_this.dataList = data.data.data;
-						_this.packageData()
-					})
-			},
 
 			// 点击各个生成器生成样例数据
 			genClick(event){
 				this.showTable = false;
 				let name = event.target.innerText;
-				let dataArr = this.dataList.date.concat(this.dataList.number, this.dataList.string, this.dataList.custom);
+				let dataArr = this.dataList.date_number.concat(this.dataList.string, this.dataList.custom);
 				dataArr.forEach(item => {
-					if(name == item.desc) {
+					if(name == item.columnTitle) {
 						this.columns.push({
 							title: name,
 							key: name,
@@ -199,12 +240,16 @@ window.onload = function() {
 							min: item.min,
 							max: item.max,
 							code: item.code,
-							scale: item.scale,
 							codeMulti: item.codeMulti,
-							className: item.className,
+							format:item.format,
+							prefix:item.prefix,
+							suffix:item.suffix,
+							name: item.name,
+							type:item.type,
 							ellipsis:true,
 							tooltip:true,
 							minWidth:100,
+							UUID: this.UUID++,
 						});
 					}
 				});
@@ -214,6 +259,11 @@ window.onload = function() {
 			deleteCol(params) {
 				this.columns.splice(params.index,1);
 				this.isDelete = false;
+				this.columns.forEach(item=>{
+					if (item.UUID > params.index) {
+						item.UUID = item.UUID-1
+					}
+				});
 				if (this.columns.length == 0){
 					this.showTable = true
 				}
@@ -221,11 +271,18 @@ window.onload = function() {
 
 			//删除全部列
 			deleteAll() {
+				if (this.columns.length == 0) {
+					this.$Message.warning({
+						content:'暂无生成器可生成，请选择所需生成器！'
+					});
+					return
+				}
 				this.$Modal.confirm({
 					title: `系统提示`,
 					content: `确认删除全部列吗？`,
 					onOk: ()=>{
 						this.columns = [];
+						this.UUID = 0;
 						this.showTable = true;
 					},
 					onCancel: ()=>{
@@ -244,65 +301,72 @@ window.onload = function() {
 
 			//封装各个模板数据
 			packageData() {
-				// 日期生成器
-				for (let i = 0 ; i < this.dataList.date.length; i++) {
+				// 数字日期生成器
+				for (let i = 0 ; i < this.dataList.date_number.length; i++) {
 					// 向dataTmp 10条对象里添加属性名值对
-					for (let j = 0 ; j < this.dataList.date[i].sampleData.length; j++) {
-						this.dataTmp[j][this.dataList.date[i].desc] = this.dataList.date[i].sampleData[j];
-					}
-				}
-				// 数字生成器
-				for (let i = 0 ; i < this.dataList.number.length ; i++) {
-					for (let j = 0 ; j < this.dataList.number[i].sampleData.length; j++) {
-						this.dataTmp[j][this.dataList.number[i].desc] = this.dataList.number[i].sampleData[j]
+					for (let j = 0 ; j < this.dataList.date_number[i].sampleData.length; j++) {
+						this.dataTmp[j][this.dataList.date_number[i].columnTitle] = this.dataList.date_number[i].sampleData[j];
 					}
 				}
 				// 字符生成器
 				for (let i = 0 ; i < this.dataList.string.length ; i++) {
 					for (let j = 0 ; j < this.dataList.string[i].sampleData.length; j++) {
-						this.dataTmp[j][this.dataList.string[i].desc] = this.dataList.string[i].sampleData[j]
+						this.dataTmp[j][this.dataList.string[i].columnTitle] = this.dataList.string[i].sampleData[j]
 					}
 				}
 				// 定制生成器
 				for (let i = 0 ; i < this.dataList.custom.length ; i++) {
 					for (let j = 0 ; j < this.dataList.custom[i].sampleData.length; j++) {
-						this.dataTmp[j][this.dataList.custom[i].desc] = this.dataList.custom[i].sampleData[j]
+						this.dataTmp[j][this.dataList.custom[i].columnTitle] = this.dataList.custom[i].sampleData[j]
 					}
 				}
 				this.data = JSON.parse(JSON.stringify(this.dataTmp));
+				console.log(this.data);
 			},
 
-			// 下载表格
-			downloadFile(val) {
-				let jsonArr = [];
-				let columnNames = [];
-				this.columns.forEach(item => {
-					jsonArr.push({
-						className: item.className,
+			// 获取当前表格列信息
+			getCurrentColumns(val) {
+				this.metaList = [];
+				val.forEach(item => {
+					this.metaList.push({
+						name: item.name,
 						min: item.min,
 						max: item.max,
 						code: item.code,
 						codeMulti: item.codeMulti,
-						scale: item.scale,
+						format:item.format,
+						prefix:item.prefix,
+						suffix:item.suffix,
+						columnKey: item.UUID,
+						columnName:item.name,
+						columnTitle:item.title,
+						type:item.type,
 					});
-					columnNames.push(item.title)
 				});
-				jsonArr = JSON.stringify(jsonArr);
-				columnNames = columnNames.join(',');
-				let params1 = `dataFormat=${val}&sampleSize=${this.downloadNumber}&metaGeneratorJsonArr=[{className:"com.hepengju.hekele.data.generator.number.IntegerGenerator",min:50,max:100},{className:"com.hepengju.hekele.data.generator.string.CodeGenerator",code:"Y,N,S,T"}]`;
-				let params = `dataFormat=${val}&sampleSize=${this.downloadNumber}&metaGeneratorJsonArr=${jsonArr}&columnNames=${columnNames}`;
-				axios.post('downloadSampleDataTable',params, {
+			},
+
+			// 下载表格
+			downloadFile(val) {
+				this.getCurrentColumns(this.columns);
+				let params = {'fileFormat':val,
+					'sampleSize':this.downloadNumber,
+					'metaList':this.metaList,
+					'tableName':'z010_user',
+					'fileName': '用户表'};
+				params = JSON.stringify(params);
+				axios.post('downTable',params, {
 					responseType: 'blob'
 				})
 			},
 
 			//获取所有数据
 			getData() {
-				let _this = this;
 				axios.get('getGenMap')
-					.then(function (data) {
-						_this.dataList = data.data.data;
-						_this.packageData()
+					.then(res => {
+						this.dataList = res.data.data;
+						console.log(this.dataList);
+						this.packageData();
+
 					})
 					.catch(function (error) {
 						console.log(error);

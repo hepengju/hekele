ce4273e95bbfc473e96b3c5fbde698bda5613d95
何鹏举 <hepengju@chengtay.com>
2020-03-11 18:06:45 +0800

    差异对比工具, 基本OK

--- 1010_base/base-nodb/src/test/java/com/hepengju/hekele/base/util/DiffUtilTest.java
+++ 1010_base/base-nodb/src/test/java/com/hepengju/hekele/base/util/DiffUtilTest.java
@@ -7,14 +7,33 @@ import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Test;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class DiffUtilTest {
 
-    @Test void toJson(){
-        Person p1 = new Person("何鹏举",30, 200, new Date(), new Pet("傻狗", 3));
-        System.out.println(JSON.toJSONString(p1));
-        System.out.println(DiffUtil.getFieldInfoList(p1));
+
+    @Test void testMap(){
+        Map<String,String> map = new HashMap<>();
+        map.put("name","何鹏举");
+        map.put("age","30");
+        System.out.println(JSON.toJSONString(map));
+        System.out.println(DiffUtil.getFieldInfoList(map));
+    }
+
+    @Test void testBase(){
+        System.out.println(DiffUtil.getFieldInfoList(10));
+    }
+
+    @Test void testGet(){
+        Person p1 = new Person("何鹏举",30, 200.0, new Date(),null, new Pet("傻狗", 3));
+        Person p2 = new Person("孙悟空",18, 200, null, LocalDate.now(), new Pet("傻狗", 10));
+        List<DiffUtil.DiffInfo> infoList = DiffUtil.get(p1, p2);
+
+        infoList.forEach(System.out::println);
     }
 
 
@@ -27,6 +46,7 @@ class DiffUtilTest {
         private double salary;
         private Date birth;
 
+        private LocalDate localDate;
         private Pet pet;
     }
 

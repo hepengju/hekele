f9b1f0088e74f189865a2583726ac94ca732c41d
何鹏举 <he_pengju@163.com>
2020-06-27 16:27:15 +0800

    尝试在zuul中加入https证书

--- 7100_zuul/src/main/java/com/hepengju/hekele/SpringZuulApp.java
+++ 7100_zuul/src/main/java/com/hepengju/hekele/SpringZuulApp.java
@@ -1,15 +1,47 @@
 package com.hepengju.hekele;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
-public class SpringZuulApp {
+public class    SpringZuulApp {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringZuulApp.class, args);
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory(){
+        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(connector());
+        return tomcat;
+    }
+
+    private Connector connector(){
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
     }
 
 }
